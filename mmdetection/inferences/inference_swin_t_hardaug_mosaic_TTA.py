cc0d7a819c34ae1698cfe1e33abce292d05cc62a
myeongu <ausrndi@gmail.com>
2022-06-13 18:02:16 +0900

    [mmdet] inference config - myeongu

---
+++ mmdetection/inferences/inference_swin_t_hardaug_mosaic_TTA.py
@@ -0,0 +1,93 @@
+import mmcv
+from mmcv import Config
+from mmdet.datasets import (build_dataloader, build_dataset,
+                            replace_ImageToTensor)
+from mmdet.models import build_detector
+from mmdet.apis import single_gpu_test
+from mmcv.runner import load_checkpoint
+import os
+from mmcv.parallel import MMDataParallel
+import pandas as pd
+from pandas import DataFrame
+from pycocotools.coco import COCO
+import numpy as np
+
+classes = ("General trash", "Paper", "Paper pack", "Metal", "Glass", 
+           "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing")
+
+#################### custom 설정들 ##########################
+## config file 들고오기 
+cfg = Config.fromfile('./configs/_test_/faster_rcnn/faster_rcnn_swin-t_pafpn_coco_augment_mosaic.py')
+
+## 원하는 pth 지정
+# epoch = 'best_bbox_mAP_epoch_23'
+epoch = 'best_bbox_mAP_epoch_28'
+
+## pth 저장 directory 지정
+cfg.work_dir = './work_dirs/faster_rcnn_swin-t_pafpn_coco_augment_mosaic'
+
+root='../../dataset/'
+
+cfg.data.samples_per_gpu = 64
+cfg.data.workers_per_gpu = 4
+############################################################
+
+# dataset config 수정
+# cfg.data.test.classes = classes
+cfg.data.test.img_prefix = root
+cfg.data.test.ann_file = root + 'test.json'
+# cfg.data.test.pipeline[1]['img_scale'] = (1024,1024) # Resize
+cfg.data.test.test_mode = True
+
+
+# cfg.seed=2021
+# cfg.gpu_ids = [1]
+
+cfg.optimizer_config.grad_clip = dict(max_norm=35, norm_type=2)
+cfg.model.train_cfg = None
+
+# # build dataset & dataloader
+dataset = build_dataset(cfg.data.test)
+data_loader = build_dataloader(
+        dataset,
+        samples_per_gpu=1,
+        workers_per_gpu=cfg.data.workers_per_gpu,
+        dist=False,
+        shuffle=False)
+
+# checkpoint path
+checkpoint_path = os.path.join(cfg.work_dir, f'{epoch}.pth')
+
+model = build_detector(cfg.model, test_cfg=cfg.get('test_cfg')) # build detector
+checkpoint = load_checkpoint(model, checkpoint_path, map_location='cpu') # ckpt load
+
+model.CLASSES = dataset.CLASSES
+model = MMDataParallel(model.cuda(), device_ids=[0])
+
+output = single_gpu_test(model, data_loader, show_score_thr=0.005) # output 계산
+
+# submission 양식에 맞게 output 후처리
+prediction_strings = []
+file_names = []
+coco = COCO(cfg.data.test.ann_file)
+img_ids = coco.getImgIds()
+
+class_num = 10
+for i, out in enumerate(output):
+    prediction_string = ''
+    image_info = coco.loadImgs(coco.getImgIds(imgIds=i))[0]
+    for j in range(class_num):
+        for o in out[j]:
+            prediction_string += str(j) + ' ' + str(o[4]) + ' ' + str(o[0]) + ' ' + str(o[1]) + ' ' + str(
+                o[2]) + ' ' + str(o[3]) + ' '
+        
+    prediction_strings.append(prediction_string)
+    file_names.append(image_info['file_name'])
+
+
+submission = pd.DataFrame()
+submission['PredictionString'] = prediction_strings
+submission['image_id'] = file_names
+
+csv_title = cfg.work_dir.split('/')[-1]
+submission.to_csv(os.path.join(cfg.work_dir, f'{csv_title}_{epoch}.csv'), index=None)
